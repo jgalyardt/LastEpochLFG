a71401ddcbf88f0199bd6a154521a37f66bc749d
Jackson Galyardt <jgalyardt@gmail.com>
2023-03-10 08:29:49 -0500

    Updated nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,22 +1,47 @@
+<script>
+export default {
+  methods: {
+
+  }
+}
+</script>
+
 <template>
-  <div class="nav row">
-    <div class="col-10">
-      <slot name="username"></slot>
-    </div>
-    <!-- <div class="col-lg-6">
-      <slot name="error"></slot>
-    </div> -->
-    <div class="col-2">
-      <slot name="hostButton"></slot>
+  <div class="nav">
+    <div class="container">
+      <div class="row">
+        <div class="col-6 float-left">
+          <img class="logo" src="../assets/LE_LFG_logo.png">
+        </div>
+        <div class="col-6 text-right">
+          <a href="https://github.com/jgalyardt/LastEpochLFG" target="_blank"><i class="fa-brands fa-git-alt"></i></a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
 .nav {
-  margin-top:-10px;
-  margin-bottom: 40px;
-  padding-top: 30px;
-  border-radius: 10px;
+  z-index: 10;
+  padding: 13px 10px 10px 10px;
+  position: fixed;
+  width: 100%;
 }
+
+.logo {
+  display: block;
+  max-height: 30px;
+}
+
+.fa-git-alt {
+  padding-top: 2px;
+  color: lightgray;
+  font-size: 24px;
+}
+
+.fa-git-alt:hover {
+  cursor: pointer;
+}
+
 </style>
